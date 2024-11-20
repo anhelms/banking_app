a7f104702bbfb71aa7c46678c7b07d908d3d30b7
Anna N Helms <unicornbuddha@gmail.com>
2024-11-20 13:23:02 -0600

    Implement plaid link and dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -15,6 +15,7 @@ import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+import PlaidLink from "./PlaidLink";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -30,35 +31,47 @@ const AuthForm = ({ type }: { type: string }) => {
       email: "",
       password: "",
     },
-  })
+  });
 
   // 2. Define a submit handler.
-   const onSubmit = async (data: z.infer<typeof formSchema>) => {
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
-    
+
     try {
-        // sign up with Appwrite & create plaid token
-        
-        if(type === 'sign-up') {
-            const newUser = await signUp(data);
+      // sign up with Appwrite & create plaid token
+      const userData = {
+        firstName: data.firstName!,
+        lastName: data.lastName!,
+        address1: data.address1!,
+        city: data.city!,
+        state: data.state!,
+        postalCode: data.postalCode!,
+        dateOfBirth: data.dateOfBirth!,
+        ssn: data.ssn!,
+        email: data.email,
+        password: data.password,
+      };
 
-            setUser(newUser);
-        }
+      if (type === "sign-up") {
+        const newUser = await signUp(userData);
 
-        if(type === 'sign-in') {
-            const response = await signIn({
-                email: data.email,
-                password: data.password,
-            })
-        
-            if(response) router.push('/')
-        }
+        setUser(newUser);
+      }
+
+      if (type === "sign-in") {
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
+
+        if (response) router.push("/");
+      }
     } catch (error) {
-        console.log(error);
+      console.log(error);
     } finally {
-        setIsLoading(false);
+      setIsLoading(false);
     }
-  }
+  };
 
   return (
     <section className="auth-form">
@@ -78,7 +91,9 @@ const AuthForm = ({ type }: { type: string }) => {
         </div>
       </header>
       {user ? (
-        <div className="flex flex-col gap-4">{/* PlaidLink */}</div>
+        <div className="flex flex-col gap-4">
+          <PlaidLink user={user} variant="primary" />
+        </div>
       ) : (
         <>
           <Form {...form}>
